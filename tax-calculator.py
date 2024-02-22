9c519108b46da29dfcecace25e5b671b19d2b5d4
z3k9 <ajelelade@gmail.com>
2024-02-22 04:17:41 +0100

    completed tax-calculator.py

---
+++ tax-calculator.py
@@ -0,0 +1,56 @@
+import customtkinter as ctk
+
+class TaxCalculator:
+    def __init__(self) -> None:
+        self.window = ctk.CTk()
+        self.window.title('Tax Calculator')
+        self.window.geometry('280x200')
+        self.window.resizable('False', 'False')
+        
+        
+        self.padding:dict ={'padx':20, 'pady':10}
+        
+        # Income label and entry
+        self.income_label = ctk.CTkLabel(self.window, text='Income')
+        self.income_label.grid(row=0, column=0, **self.padding)
+        self.income_entry = ctk.CTkEntry(self.window)
+        self.income_entry.grid(row=0, column=1, **self.padding)
+        
+        #Tax label and entry
+        self.tax_label = ctk.CTkLabel(self.window, text='Percent')
+        self.tax_label.grid(row=1, column=0, **self.padding)
+        self.tax_entry = ctk.CTkEntry(self.window)
+        self.tax_entry.grid(row=1, column=1, **self.padding)
+        
+        # Result label and entry
+        self.result_label = ctk.CTkLabel(self.window, text='Tax')
+        self.result_label.grid(row=2, column=0, **self.padding)
+        self.result_entry = ctk.CTkEntry(self.window)
+        self.result_entry.insert(0, '0')
+        self.result_entry.grid(row=2, column=1, **self.padding)
+        
+        
+        # Calculate button
+        self.calculate_button =ctk.CTkButton(self.window, text='Calculate', command=self.calculate_tax)
+        self.calculate_button.grid(row=3, column=1, **self.padding)
+        
+        
+    def update_result(self, text:str):
+        self.result_entry.delete(0, ctk.END)
+        self.result_entry.insert(0,text)
+    
+    def calculate_tax(self):
+        try:
+            income:float = float(self.income_entry.get())
+            tax_rate:float = float(self.tax_entry.get())
+            self.update_result(f'${income  * (tax_rate/100):,.2f}')
+        
+        except ValueError:
+            self.update_result('Invalid Input')
+            
+    def run(self):
+        self.window.mainloop()
+
+if __name__ =='__main__':
+    tc = TaxCalculator()
+    tc.run()
